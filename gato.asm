0650af04d3ac3fd296baf4ed13b155f68d9d7657
Albin Gabriel Monge Arias <gabriel12mx@gmail.com>
2024-06-23 23:48:42 -0600

    Perfil de cada jugador (color)

--- gato.asm
+++ gato.asm
@@ -1,13 +1,18 @@
 .data
 	display: .space 1024
 	casilla: .space 40
+	colores_hex: .word 0x00ffb6, 0xfff700, 0x8300ff, 0xff2a00 #(incluir cuantos se desee)
+	colores: .asciiz "Colores a escoger:\n1) Cian\n2) Amarrillo\n3) Morado\n4) Naranja\n"
+	j1: .asciiz "Jugador 1: "
+	j2: .asciiz "Jugador 2: "
+	seleccion: .asciiz "\nEscoja un n�mero de casilla [1, 9]: "
 	
 .text
 	main:
 		li $a1, 0xffffff
 		jal crear_tablero
-		la $a0, display($t0)
-		jal casillas 
+		la $s0, display($zero)
+		jal casillas
 		jal juego
 		jal resultado
 		j end
@@ -42,8 +47,8 @@
       j vertical
       
   casillas: #direcci�n de memoria de las casillas a marcar, indexable luego con (n�mero de casilla * 4) + direcci�n inicial del space casilla.
-		addi $t0, $a0, 136 #primera linea de pixeles de seleccion
-		addi $t2, $a0, 776 #ultima linea
+		addi $t0, $s0, 136 #primera linea de pixeles de seleccion
+		addi $t2, $s0, 776 #ultima linea
 		la $t3, casilla
 		
 		new_line: 
@@ -59,9 +64,35 @@
 		j new_line
 		
   juego:
-  	perfil: #se le pide a cada jugador escoger un color de dos disponibles (opcional)
-  	entrada: #numero de casilla, codificar a direcci�n de memoria 
-  	valido: #revizar casilla si esta disponible
+  	perfil: #se le pide a cada jugador escoger un color de los disponibles
+  		move $t3, $ra #conservar el retorno a main
+  		la $t1, colores_hex
+  		la $a0, colores #msg colores disponinles
+  		li $v0, 4
+  		syscall
+  		
+  		la $a0, j1 #primer color 
+  		jal selec_color
+  		move $s1, $v1
+  		segundo: la $a0, j2 #segundo color 
+  		jal selec_color
+  		beq $s1, $t0, segundo #cada jugador debe tener un color distinto
+  		move $s2, $v1 
+  		move $ra, $t3
+  		j ret
+  		
+  		selec_color:
+  			li $v0, 4
+  			syscall
+  			li $v0, 5
+  			syscall
+  			sub $t0, $v0, 1
+  			mul $t0, $t0, 4
+  			add $t2, $t1, $t0   
+  			lw $v1, 0($t2) # codigo del color escogido
+  			j ret
+
+  	entrada: #numero de casilla, codificar a direcci�n de memoria
   	gane: #definir si hay ganador, ir a "ret" o a "continuar" *
   	continuar: #reviza si hay al menos una casilla disponible, ir a "ret" o a "entrada"
 
