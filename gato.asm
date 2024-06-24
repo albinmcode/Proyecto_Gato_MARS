bdbe479c4b9d19a60237f5261c7cb749b7741724
Albin Gabriel Monge Arias <gabriel12mx@gmail.com>
2024-06-23 18:19:59 -0600

    Selección y marcado de casilla
    Previamente se debe cargar en $a1 el color de jugador del turno correspondiente (borrar primera instrucción).

--- gato.asm
+++ gato.asm
@@ -1,12 +1,13 @@
 .data
 	display: .space 1024
 	casilla: .space 40
+	seleccion: .asciiz "Escoja un número de casilla [1, 9]: "
 	
 .text
 	main:
 		li $a1, 0xffffff
 		jal crear_tablero
-		la $a0, display($t0)
+		la $s0, display($zero)
 		jal casillas 
 		jal juego
 		jal resultado
@@ -42,8 +43,8 @@
       j vertical
       
   casillas: #direcciï¿½n de memoria de las casillas a marcar, indexable luego con (nï¿½mero de casilla * 4) + direcciï¿½n inicial del space casilla.
-		addi $t0, $a0, 136 #primera linea de pixeles de seleccion
-		addi $t2, $a0, 776 #ultima linea
+		addi $t0, $s0, 136 #primera linea de pixeles de seleccion
+		addi $t2, $s0, 776 #ultima linea
 		la $t3, casilla
 		
 		new_line: 
@@ -60,8 +61,30 @@
 		
   juego:
   	perfil: #se le pide a cada jugador escoger un color de dos disponibles (opcional)
-  	entrada: #numero de casilla, codificar a direcciï¿½n de memoria 
-  	valido: #revizar casilla si esta disponible
+  	entrada: #numero de casilla, codificar a direcciï¿½n de memoria
+  		move $a1, $s1 #asignacion al inicio de un turno especifico(funcion de rondas)
+  		sw $a1, display($zero)
+  		#lw $t0, 0xffff0004 #entrada de teclado
+  		la $a0, seleccion
+  		li $v0, 4
+  		syscall
+  		li $v0, 5
+  		syscall
+  		mul $t0, $v0, 4 
+  		lw $t1, casilla($t0)
+  		validez:
+  			lw $t0, ($t1) #color actual de la casilla
+  			bne $t0, 0xffffff, entrada #si tiene un color distinto al de fondo se vuelve a pedir otra posicion
+  			addi $s3, $s3, 1 #contador de casillas marcadas
+  		marcar:
+  			sw $a1, ($t1)
+  			addi $t1, $t1, 4
+  			sw $a1, ($t1)
+				addi $t1, $t1, 60
+  			sw $a1, ($t1)
+  			addi $t1, $t1, 4
+  			sw $a1, ($t1)  		
+  	
   	gane: #definir si hay ganador, ir a "ret" o a "continuar" *
   	continuar: #reviza si hay al menos una casilla disponible, ir a "ret" o a "entrada"
 
